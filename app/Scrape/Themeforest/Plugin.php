9c2637e3969820335aa7592a2dbb37579dedc3e8
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-04-02 22:13:21 +0400

    better formatting

--- app/Scrape/Themeforest/Plugin.php
+++ app/Scrape/Themeforest/Plugin.php
@@ -55,107 +55,101 @@ class Plugin {
 		$plugin = [];
 
 
-		$crawler->filter( 'li.js-google-analytics__list-event-container' )->each( function ( $themelist ) use ( &$plugin
-		) {
+		$crawler->filter( 'li.js-google-analytics__list-event-container' )
+		        ->each( function ( $themelist ) use ( &$plugin ) {
 
 
-			// The theme Unique id
-			$plugin['id'] = $themelist->attr( 'data-item-id' );
+			        // The theme Unique id
+			        $plugin['id'] = $themelist->attr( 'data-item-id' );
 
-			// The theme name
-			$themelist->filter( 'h3' )->each( function ( $themeTitle ) use ( &$plugin
-			) {
-				$plugin['name'] = $themeTitle->text();
+			        // The theme name
+			        $themelist->filter( 'h3' )
+			                  ->each( function ( $themeTitle ) use ( &$plugin ) {
+				                  $plugin['name'] = $themeTitle->text();
+			                  } );
 
-			} );
+			        // The theme preview  screenshot
+			        $themelist->filter( 'img.preload' )
+			                  ->each( function ( $themeImage ) use ( &$plugin ) {
+				                  $plugin['previewScreenshot'] = $themeImage->attr( 'data-preview-url' );
+			                  } );
 
-			// The theme preview  screenshot
-			$themelist->filter( 'img.preload' )->each( function ( $themeImage ) use ( &$plugin
-			) {
-				$plugin['previewScreenshot'] = $themeImage->attr( 'data-preview-url' );
-			} );
-
-			// The theme preview  screenshot
-			$themelist->filter( '[itemprop="genre"]' )->each( function ( $themeImage ) use ( &$plugin
-			) {
-				$plugin['category'] = $themeImage->text();
-			} );
-
-
-			// Click on each theme name and go to their theme page details
-			if ( ! empty( trim( $plugin['name'] ) ) ) {
-
-				try {
-					$link                 = $this->crawler->selectLink( trim( $plugin['name'] ) )->link();
-					$crawlerThemefullPage = $this->client->click( $link );
+			        // The theme preview  screenshot
+			        $themelist->filter( '[itemprop="genre"]' )
+			                  ->each( function ( $themeImage ) use ( &$plugin ) {
+				                  $plugin['category'] = $themeImage->text();
+			                  } );
 
 
-					// Get the Preview URL
-					$crawlerThemefullPage->filter( 'a.live-preview' )->each( function ( $themePreviewlink ) use (
-						&
-						$plugin
-					) {
-						$plugin['previewURL'] = $themePreviewlink->attr( 'href' );
-					} );
+			        // Click on each theme name and go to their theme page details
+			        if ( ! empty( trim( $plugin['name'] ) ) ) {
+
+				        try {
+					        $link                 = $this->crawler->selectLink( trim( $plugin['name'] ) )->link();
+					        $crawlerThemefullPage = $this->client->click( $link );
 
 
-					// Get the theme description
-					$crawlerThemefullPage->filter( 'div.item-description' )->each( function ( $themeDescription ) use (
-						&$plugin
-					) {
-						$plugin['description'] = $themeDescription->text();
-					} );
-
-					// Click on the preview link
-					$previewlink             = $crawlerThemefullPage->selectLink( 'Live Preview' )->link();
-					$crawlerThemePreviewLink = $this->client->click( $previewlink );
-
-					// Get the theme url hosted by the author
-					$crawlerThemePreviewLink->filter( 'div.preview__action--close a' )->each( function (
-						$themeDescription
-					) use ( &$plugin
-					) {
-						$plugin['origin'] = $themeDescription->attr( 'href' );
-					} );
+					        // Get the Preview URL
+					        $crawlerThemefullPage->filter( 'a.live-preview' )
+					                             ->each( function ( $themePreviewlink ) use ( & $plugin ) {
+						                             $plugin['previewURL'] = $themePreviewlink->attr( 'href' );
+					                             } );
 
 
-					if ( $this->exist( trim( $plugin['id'] ) ) ) {
-						$pluginModel                   = new PluginModel;
-						$pluginModel->uniqueidentifier = trim( $plugin['id'] );
-						$pluginModel->name             = trim( $plugin['name'] );
-						$pluginModel->url              = trim( $plugin['previewURL'] );
-						$pluginModel->downloadlink     = trim( $plugin['previewURL'] );
-						$pluginModel->demolink         = trim( $plugin['origin'] );
-						$pluginModel->description      = trim( $plugin['description'] );
-						$pluginModel->screenshotUrl    = trim( $plugin['previewScreenshot'] );
-						$pluginModel->provider         = 'themeforest';
-						$pluginModel->category         = trim( $plugin['category'] );
-						$pluginModel->type             = 'premium';
-						$pluginModel->save();
-					}
+					        // Get the theme description
+					        $crawlerThemefullPage->filter( 'div.item-description' )
+					                             ->each( function ( $themeDescription ) use ( &$plugin ) {
+						                             $plugin['description'] = $themeDescription->text();
+					                             } );
+
+					        // Click on the preview link
+					        $previewlink             = $crawlerThemefullPage->selectLink( 'Live Preview' )->link();
+					        $crawlerThemePreviewLink = $this->client->click( $previewlink );
+
+					        // Get the theme url hosted by the author
+					        $crawlerThemePreviewLink->filter( 'div.preview__action--close a' )
+					                                ->each( function ( $themeDescription ) use ( &$plugin ) {
+						                                $plugin['origin'] = $themeDescription->attr( 'href' );
+					                                } );
 
 
-					$this->result[] = [
-						'uniqueidentifier'  => trim( $plugin['id'] ),
-						'name'              => trim( $plugin['name'] ),
-						'previewScreenshot' => trim( $plugin['previewScreenshot'] ),
-						'previewURL'        => trim( $plugin['previewURL'] ),
-						'description'       => trim( $plugin['description'] ),
-						'origin'            => trim( $plugin['origin'] ),
-						'category'          => trim( $plugin['category'] ),
-					];
+					        if ( $this->exist( trim( $plugin['id'] ) ) ) {
+						        $pluginModel                   = new PluginModel;
+						        $pluginModel->uniqueidentifier = trim( $plugin['id'] );
+						        $pluginModel->name             = trim( $plugin['name'] );
+						        $pluginModel->url              = trim( $plugin['previewURL'] );
+						        $pluginModel->downloadlink     = trim( $plugin['previewURL'] );
+						        $pluginModel->demolink         = trim( $plugin['origin'] );
+						        $pluginModel->description      = trim( $plugin['description'] );
+						        $pluginModel->screenshotUrl    = trim( $plugin['previewScreenshot'] );
+						        $pluginModel->provider         = 'themeforest';
+						        $pluginModel->category         = trim( $plugin['category'] );
+						        $pluginModel->type             = 'premium';
+						        $pluginModel->save();
+					        }
 
 
-				} catch ( \InvalidArgumentException $e ) {
-					echo "Well, it sucks. Cannot scrape: " . json_encode( $plugin['id'] );
-					echo "<br/> \n";
-				}
-			} else {
-				echo "No data for" . $plugin['id'];
-				echo "<br/> \n";
-			}
+					        $this->result[] = [
+						        'uniqueidentifier'  => trim( $plugin['id'] ),
+						        'name'              => trim( $plugin['name'] ),
+						        'previewScreenshot' => trim( $plugin['previewScreenshot'] ),
+						        'previewURL'        => trim( $plugin['previewURL'] ),
+						        'description'       => trim( $plugin['description'] ),
+						        'origin'            => trim( $plugin['origin'] ),
+						        'category'          => trim( $plugin['category'] ),
+					        ];
 
-		} );
+
+				        } catch ( \InvalidArgumentException $e ) {
+					        echo "Well, it sucks. Cannot scrape: " . json_encode( $plugin['id'] );
+					        echo "<br/> \n";
+				        }
+			        } else {
+				        echo "No data for" . $plugin['id'];
+				        echo "<br/> \n";
+			        }
+
+		        } );
 
 
 		//return $this->result;
