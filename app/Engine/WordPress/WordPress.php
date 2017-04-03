ada0a7fbfa35a20e04de4709125289498447767d
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-04-03 23:57:13 +0400

    Improve engines by extracting classes and ids

---
+++ app/Engine/WordPress/WordPress.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Engine\WordPress;
+
+/**
+ * Created by PhpStorm.
+ * User: Hyder
+ * Date: 03/04/2017
+ * Time: 22:16
+ */
+class WordPress {
+
+	/**
+	 * 1. Check for presence of readme.html
+	 * 2. Check for generator meta tags
+	 * 3. Check presence of this file: https://darktips.com/wp-includes/css/buttons.css
+	 * 4. Check for WP core api: wp-json in url
+	 * 5. check for wp-content/wp-includes in souces code
+	 * 6.
+	 * 7.
+	 * 8.
+	 * 9.
+	 * 10.
+	 * 11.
+	 * 12.
+	 * 13.
+	 * 14.
+	 * 15.
+	 */
+
+}
