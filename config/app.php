e8ec8d92c9764a31e9178c0e55047fd27dbca314
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-04-04 14:11:32 +0400

    Add goutte as facade

--- config/app.php
+++ config/app.php
@@ -228,6 +228,7 @@ return [
 		'Validator'    => Illuminate\Support\Facades\Validator::class,
 		'View'         => Illuminate\Support\Facades\View::class,
 		'Bugsnag'      => Bugsnag\BugsnagLaravel\Facades\Bugsnag::class,
+		'Goutte'       => App\Facades\GoutteFacade::class,
 	],
 
 ];
