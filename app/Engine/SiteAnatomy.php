5d82bb985e7809708f90d9b28394308f370bc37c
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-04-11 21:28:21 +0400

    PSR'd

--- app/Engine/SiteAnatomy.php
+++ app/Engine/SiteAnatomy.php
@@ -10,222 +10,236 @@ use App;
  * Date: 03/04/2017
  * Time: 10:52
  */
-class SiteAnatomy {
+class SiteAnatomy
+{
 
 
-	/**
-	 * @var \Symfony\Component\DomCrawler\Crawler
-	 */
-	private $crawler;
-	public $styles;
-	public $scripts;
-	public $metas;
-	public $headers;
-	public $cookies;
-	public $comments;
-	public $status;
-	public $css;
-	public $innerlinks;
+    /**
+     * @var \Symfony\Component\DomCrawler\Crawler
+     */
+    public $crawler;
+    public $styles;
+    public $scripts;
+    public $metas;
+    public $headers;
+    public $cookies;
+    public $comments;
+    public $status;
+    public $css;
+    public $innerlinks;
 
 
-	public function __construct( $site ) {
+    public function __construct($site)
+    {
 
-		$this->goutteClient = App::make( 'goutte' );
+        $this->goutteClient = App::make('goutte');
 
-		$this->crawler = $this->goutteClient->request(
-			'GET',
-			"http://" . $site
-		);
+        $this->crawler = $this->goutteClient->request(
+            'GET',
+            "http://" . $site
+        );
 
-		$this->result();
+        $this->result();
 
-	}
-
-
-	/**
-	 * Get the raw HTML
-	 * @return null|object
-	 */
-	private function getHtml() {
-		return $this->goutteClient->getResponse()->getContent();
-	}
-
-	/**
-	 * Get meta tags
-	 * @return array
-	 */
-	private function metatags() {
-
-		$tags = [];
-		$this->crawler->filterXpath( '//meta[@name="generator"]' )
-		              ->each( function ( $metaTags ) use ( &$tags ) {
-
-			              $tags['generator'][] = $metaTags->attr( 'content' );
-
-		              } );
-
-		return $tags;
-
-	}
-
-	/**
-	 * Get HTTP Headers
-	 * @return mixed
-	 */
-	private function getHeaders() {
-		return $this->goutteClient->getResponse()->getHeaders();
-	}
+    }
 
 
-	/**
-	 * Get HTTP Response code
-	 * @return mixed
-	 */
-	private function getStatus() {
-		return $this->goutteClient->getResponse()->getStatus();
-	}
+    /**
+     * Get the raw HTML
+     * @return null|object
+     */
+    private function getHtml()
+    {
+        return $this->goutteClient->getResponse()->getContent();
+    }
 
-	/**
-	 * Get cookie jar
-	 * @return \Symfony\Component\BrowserKit\CookieJar
-	 */
-	private function getCookies() {
-		return $this->goutteClient->getCookieJar();
-	}
+    /**
+     * Get meta tags
+     * @return array
+     */
+    private function metatags()
+    {
 
+        $tags = [];
+        $this->crawler->filterXpath('//meta[@name="generator"]')
+                      ->each(function ($metaTags) use (&$tags) {
 
-	/**
-	 * List CSS Sheets
-	 * @return array
-	 */
-	private function getStyleSheets() {
-		$styleSheets = [];
-		$this->crawler->filterXpath( '//link[@rel="stylesheet"]' )
-		              ->each( function ( $styleSheet ) use ( &$styleSheets ) {
+                          $tags['generator'][] = $metaTags->attr('content');
 
-			              $styleSheets[] = $styleSheet->attr( 'href' );
+                      });
 
-		              } );
+        return $tags;
 
-		return array_unique( $styleSheets );
-	}
+    }
 
-	/**
-	 * List JS scripts
-	 * @return array
-	 */
-	private function getScripts() {
-		$scripts = [];
-		$this->crawler->filterXpath( '//script' )
-		              ->each( function ( $script ) use ( &$scripts ) {
-			              if ( ! is_null( $script->attr( 'src' ) ) ) {
-				              $scripts[] = $script->attr( 'src' );
-			              }
-		              } );
-
-		return array_unique( $scripts );
-	}
-
-	/**
-	 * Parse HTML Comments
-	 * @return array
-	 */
-	private function getHtmlComments() {
-
-		if ( preg_match_all( '/<!-- ([\\s\\S])*?-->/uim', $this->getHtml(), $comments ) ) {
-			return $comments[0];
-		}
+    /**
+     * Get HTTP Headers
+     * @return mixed
+     */
+    private function getHeaders()
+    {
+        return $this->goutteClient->getResponse()->getHeaders();
+    }
 
 
-	}
+    /**
+     * Get HTTP Response code
+     * @return mixed
+     */
+    private function getStatus()
+    {
+        return $this->goutteClient->getResponse()->getStatus();
+    }
+
+    /**
+     * Get cookie jar
+     * @return \Symfony\Component\BrowserKit\CookieJar
+     */
+    private function getCookies()
+    {
+        return $this->goutteClient->getCookieJar();
+    }
 
 
-	/**
-	 * Extract CSS Class names
-	 * @return array
-	 */
-	private function getCssClasses() {
+    /**
+     * List CSS Sheets
+     * @return array
+     */
+    private function getStyleSheets()
+    {
+        $styleSheets = [];
+        $this->crawler->filterXpath('//link[@rel="stylesheet"]')
+                      ->each(function ($styleSheet) use (&$styleSheets) {
 
-		$cssClasses = [];
-		$this->crawler->filterXpath( '//*[@class]' )
-		              ->each( function ( $cssClass ) use ( &$cssClasses ) {
+                          $styleSheets[] = $styleSheet->attr('href');
 
-			              $classes      = trim( $cssClass->attr( 'class' ) );
-			              $classes      = explode( ' ', $classes );
-			              $cssClasses[] = $classes;
+                      });
 
-		              } );
+        return array_unique($styleSheets);
+    }
 
-		$uniqueCssClasses = array_unique( array_flatten( $cssClasses ) );
+    /**
+     * List JS scripts
+     * @return array
+     */
+    private function getScripts()
+    {
+        $scripts = [];
+        $this->crawler->filterXpath('//script')
+                      ->each(function ($script) use (&$scripts) {
+                          if ( ! is_null($script->attr('src'))) {
+                              $scripts[] = $script->attr('src');
+                          }
+                      });
 
-		$finalCssClasses = [];
-		foreach ( $uniqueCssClasses as $uniqueCssClass ) {
-			if ( strlen( $uniqueCssClass ) > 5 ) {
-				$finalCssClasses[] = $uniqueCssClass;
-			}
-		}
+        return array_unique($scripts);
+    }
 
+    /**
+     * Parse HTML Comments
+     * @return array
+     */
+    private function getHtmlComments()
+    {
 
-		return $finalCssClasses;
-
-	}
-
-	/**
-	 * Extract CSS Ids
-	 * @return array
-	 */
-	private function getCssIds() {
-
-		$cssIds = [];
-		$this->crawler->filterXpath( '//*[@id]' )
-		              ->each( function ( $cssId ) use ( &$cssIds ) {
-
-			              $cssIds[] = trim( $cssId->attr( 'id' ) );
-
-		              } );
-
-		$uniqueCssIds = array_unique( array_flatten( $cssIds ) );
-
-		$finalCssIds = [];
-		foreach ( $uniqueCssIds as $uniqueCssId ) {
-			if ( strlen( $uniqueCssId ) > 5 ) {
-				$finalCssIds[] = $uniqueCssId;
-			}
-		}
-
-		return $finalCssIds;
-	}
-
-	/**
-	 * Find inner links of a domain to make further analysis
-	 */
-	private function getInnnerLinks() {
-
-	}
+        if (preg_match_all('/<!-- ([\\s\\S])*?-->/uim', $this->getHtml(), $comments)) {
+            return $comments[0];
+        }
 
 
-	/**
-	 * Get the result
-	 * @return array
-	 */
-	private function result() {
-
-		$this->styles     = $this->getStyleSheets();
-		$this->scripts    = $this->getScripts();
-		$this->metas      = $this->metatags();
-		$this->headers    = $this->getHeaders();
-		$this->cookies    = $this->getCookies();
-		$this->comments   = $this->getHtmlComments();
-		$this->status     = $this->getStatus();
-		$this->css        = [
-			'classes' => $this->getCssClasses(),
-			'ids'     => $this->getCssIds(),
-		];
-		$this->innerlinks = $this->getInnnerLinks();
-		$this->html       = $this->getHtml();
+    }
 
 
-	}
+    /**
+     * Extract CSS Class names
+     * @return array
+     */
+    private function getCssClasses()
+    {
+
+        $cssClasses = [];
+        $this->crawler->filterXpath('//*[@class]')
+                      ->each(function ($cssClass) use (&$cssClasses) {
+
+                          $classes      = trim($cssClass->attr('class'));
+                          $classes      = explode(' ', $classes);
+                          $cssClasses[] = $classes;
+
+                      });
+
+        $uniqueCssClasses = array_unique(array_flatten($cssClasses));
+
+        $finalCssClasses = [];
+        foreach ($uniqueCssClasses as $uniqueCssClass) {
+            if (strlen($uniqueCssClass) > 5) {
+                $finalCssClasses[] = $uniqueCssClass;
+            }
+        }
+
+
+        return $finalCssClasses;
+
+    }
+
+    /**
+     * Extract CSS Ids
+     * @return array
+     */
+    private function getCssIds()
+    {
+
+        $cssIds = [];
+        $this->crawler->filterXpath('//*[@id]')
+                      ->each(function ($cssId) use (&$cssIds) {
+
+                          $cssIds[] = trim($cssId->attr('id'));
+
+                      });
+
+        $uniqueCssIds = array_unique(array_flatten($cssIds));
+
+        $finalCssIds = [];
+        foreach ($uniqueCssIds as $uniqueCssId) {
+            if (strlen($uniqueCssId) > 5) {
+                $finalCssIds[] = $uniqueCssId;
+            }
+        }
+
+        return $finalCssIds;
+    }
+
+    /**
+     * Find inner links of a domain to make further analysis
+     */
+    private function getInnnerLinks()
+    {
+
+    }
+
+
+    /**
+     * Get the result
+     * @return array
+     */
+    private function result()
+    {
+
+        $this->styles     = $this->getStyleSheets();
+        $this->scripts    = $this->getScripts();
+        $this->metas      = $this->metatags();
+        $this->headers    = $this->getHeaders();
+        $this->cookies    = $this->getCookies();
+        $this->comments   = $this->getHtmlComments();
+        $this->status     = $this->getStatus();
+        $this->css        = [
+            'classes' => $this->getCssClasses(),
+            'ids'     => $this->getCssIds(),
+        ];
+        $this->innerlinks = $this->getInnnerLinks();
+        $this->html       = $this->getHtml();
+
+
+    }
 
 
 }
