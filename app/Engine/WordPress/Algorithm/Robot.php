5cd5dc2a5282070b76d6b5d249f8f99e20b2fa4d
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-04-11 21:27:35 +0400

    Algorithm: Check in robots.txt

--- app/Engine/WordPress/Algorithm/Robot.php
+++ app/Engine/WordPress/Algorithm/Robot.php
@@ -7,21 +7,72 @@
  */
 
 namespace App\Engine\WordPress\Algorithm;
+
 use App\Engine\WordPress\WordPressAbstract;
 
 use App\Engine\SiteAnatomy;
 
-class Robot extends WordPressAbstract {
-// Check presence of wp-content in robots.txt
-// Disallow: /wp-admin/
-// Allow: /wp-admin/admin-ajax.php
+class Robot extends WordPressAbstract
+{
 
-	public function check( SiteAnatomy $siteAnatomy ) {
+    /**
+     * Inject an instance of \App\Engine\SiteAnatomy
+     * @var
+     */
+    public $siteAnatomy;
 
-		$this->setScore( '0', "Well, no footprint in robots.txt" );
 
-		return $this;
-	}
+    /**
+     * The path to Robots.txt
+     * @var
+     */
+    public $pathToRobotsTxt;
+
+    /**
+     * @param SiteAnatomy $siteAnatomy
+     *
+     * @return $this
+     */
+    public function check(SiteAnatomy $siteAnatomy)
+    {
+
+        $this->siteAnatomy     = $siteAnatomy;
+        $host                  = parse_url($this->siteAnatomy->crawler->getBaseHref(), PHP_URL_HOST);
+        $this->pathToRobotsTxt = "http://$host/robots.txt";
+        $content               = $this->getRobotsTxtContent();
+        $this->checkWordPressFootprints($content);
+
+        return $this;
+    }
+
+
+    /**
+     * Fetch the content of the Robots.txt
+     */
+    public function getRobotsTxtContent()
+    {
+        $goutteClient = \App::make('goutte');
+
+        $goutteClient->request(
+            'GET',
+            $this->pathToRobotsTxt
+        );
+
+        return $content = $goutteClient->getResponse()->getContent();
+
+    }
+
+    /**
+     * Check for WordPress footprints in robots.txt
+     *
+     * @param $content
+     */
+    public function checkWordPressFootprints($content)
+    {
+        if (preg_match('/(wp-admin|admin-ajax.php|wp-content)/i', $content, $matches)) {
+            $this->assertWordPress('robot');
+        }
+    }
 
 
 }
