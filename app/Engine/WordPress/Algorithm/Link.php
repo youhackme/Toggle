998eadf7abd4da352be200678293f2b742ba3ba3
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-04-12 20:05:55 +0400

    Lay out the basic idea..

--- app/Engine/WordPress/Algorithm/Link.php
+++ app/Engine/WordPress/Algorithm/Link.php
@@ -7,25 +7,53 @@
  */
 
 namespace App\Engine\WordPress\Algorithm;
+
 use App\Engine\WordPress\WordPressAbstract;
 
 use App\Engine\SiteAnatomy;
 
 
-class Link extends WordPressAbstract {
+class Link extends WordPressAbstract
+{
 
 //// check presence of absolute path such as readme, buttons.css , license.txt
-	///wp-includes/wlwmanifest.xml
-	// WordPress version : wp-links-opml.php
-	// /wp-json/
+    ///wp-includes/wlwmanifest.xml
+    // WordPress version : wp-links-opml.php
+    // /wp-json/
+    //- It should be asynchronous
 
 
-	public function check( SiteAnatomy $siteAnatomy ) {
+    public function check(SiteAnatomy $siteAnatomy)
+    {
 
-		$this->setScore( '0', "Well, no footprint in links" );
+        $this->setScore('0', "Well, no footprint in links");
 
-		return $this;
-	}
+        return $this;
+    }
+
+    private function commonWordPressPath()
+    {
+        return [
+            [
+                'path'      => 'readme.html',
+                'searchFor' => '/WordPress/',
+            ],
+            [
+                'path'      => 'wp-includes/wlwmanifest.xml',
+                'searchFor' => '/WordPress|manifest/',
+            ],
+            [
+                'path'      => 'wp-links-opml.php',
+                'searchFor' => '/WordPress|opml/',
+            ],
+            [
+                'path'      => 'wp-json',
+                'searchFor' => '/WordPress|wp-api/',
+            ],
+
+
+        ];
+    }
 
 
 }
