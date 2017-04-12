576dbf080d4d1fdb123da4b859cacf5ac737770f
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-04-12 20:05:14 +0400

    How WP version should be determined

---
+++ app/Engine/WordPress/Algorithm/Version.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hyder
+ * Date: 12/04/2017
+ * Time: 19:56
+ */
+
+namespace App\Engine\WordPress\Algorithm;
+
+
+class Version
+{
+
+    //Algorithm for finding WordPress version
+    // 1. https://darktips.com/wp-links-opml.php
+    // 2. wp-includes/wlwmanifest.xml
+    // 3. In meta tag
+    // 4. from source code in login page E.g load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.7.3
+}
