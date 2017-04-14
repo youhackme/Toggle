fb49990e2faf0c6257796f6e7ba57c35fe580081
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-04-14 20:17:03 +0400

    A work to be continued..

---
+++ app/Engine/WordPress/Algorithm/HideMyWp.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hyder
+ * Date: 13/04/2017
+ * Time: 21:07
+ */
+
+namespace App\Engine\WordPress\Algorithm;
+
+
+class HideMyWp
+{
+    // Check Presence of HideMyWP
+// /wp-content/plugins/hide-my-wp/documentation/index.html
+// /wp-content/plugins/hide-my-wp/js/admin.js
+
+}
