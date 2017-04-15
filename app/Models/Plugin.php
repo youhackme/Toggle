c1770077b8fdcdc5ebd50b0b11611b6e273dd6af
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-04-15 14:03:42 +0400

    Added mutators to plugin model

--- app/Models/Plugin.php
+++ app/Models/Plugin.php
@@ -4,7 +4,46 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Plugin extends Model {
+class Plugin extends Model
+{
 
-	protected $table = 'plugins';
+    protected $table = 'plugins';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'uniqueidentifier',
+        'name',
+        'screenshotUrl',
+        'category',
+        'demolink',
+        'description',
+        'provider',
+        'type',
+        'url',
+        'downloadlink',
+
+    ];
+
+
+    public function setuniqueidentifierAttribute($value)
+    {
+        $this->attributes['uniqueidentifier'] = trim(strtolower($value));
+    }
+
+    public function setnameAttribute($value)
+    {
+        $this->attributes['name'] = trim(strtolower($value));
+    }
+
+    public function setdescriptionAttribute($value)
+    {
+        $this->attributes['description'] = trim(strtolower($value));
+    }
+
+
 }
