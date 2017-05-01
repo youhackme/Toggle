c67f995b565248c333f81618a88bc8ab3d2b4ec1
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-05-01 14:21:25 +0400

    Added mass assignment to certain fields

--- app/Models/ThemeMeta.php
+++ app/Models/ThemeMeta.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class ThemeMeta extends Model
 {
     protected $table = 'thememeta';
+
+    protected $fillable = [
+        'themeid',
+        'slug',
+        'screenshotExternalUrl',
+        'screenshotHash',
+        'authorname',
+        'authorurl',
+        'status',
+    ];
+
+
+    public function setslugAttribute($value)
+    {
+        $this->attributes['slug'] = trim(strtolower($value));
+    }
+
 }
