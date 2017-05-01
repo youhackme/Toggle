ceaba2eee652cc655f65a61fc9d114b728e8abb5
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-05-02 00:02:02 +0400

    Improve error handling

--- app/Engine/WordPress/Algorithm/Screenshot.php
+++ app/Engine/WordPress/Algorithm/Screenshot.php
@@ -69,11 +69,17 @@ class Screenshot extends WordPressAbstract
     public function urlExist($screenshotUrl)
     {
         $goutteClient = \App::make('goutte');
-        $goutteClient->request(
-            'GET',
-            $screenshotUrl
-        );
+        try {
+            $goutteClient->request('GET', $screenshotUrl);
 
-        return $goutteClient->getResponse()->getStatus();
+            return $goutteClient->getResponse()->getStatus();
+
+        } catch (\GuzzleHttp\Exception\ConnectException $e) {
+
+            echo $e->getMessage();
+
+            return false;
+        }
+
     }
 }
