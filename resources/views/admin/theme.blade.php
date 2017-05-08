a05570ade572394fd9f9098323b3ac3a1e93e215
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-05-08 21:56:02 +0400

    Initial views to add a theme manually

---
+++ resources/views/admin/theme.blade.php
@@ -0,0 +1,34 @@
+<div class="container">
+    <div class="col-md-offset-3 col-md-6">
+        <form role="form">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Theme demo Url</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="http://toggle.me">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Theme Name</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Avada">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleInputEmail1">Theme Description</label>
+                <textarea type="text" class="form-control" id="exampleInputEmail1" placeholder="This is a bootstrap 4 premium theme."></textarea>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Theme Alias</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="avada">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Screenshot Url</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="http://toggle.me/wp-content/themes/avada/screenshot.png">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Screenshot Hash</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="123abef67676aeb3abcds">
+            </div>
+
+
+            <button type="submit" class="btn btn-info btn-block">Submit</button>
+        </form>
+    </div>
+</div>
