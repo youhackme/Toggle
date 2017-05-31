64143f5ab3eea97a70c7802284d4232b714a4577
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-05-31 14:07:40 +0400

    Scrape css igniter

---
+++ app/Scrape/CssIgniter/Plugin.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Scrape\CssIgniter;
+
+use App\Scrape\ScraperInterface;
+use Symfony\Component\DomCrawler\Crawler;
+use App\Repositories\Plugin\PluginRepository;
+
+/**
+ * Created by PhpStorm.
+ * User: Hyder
+ * Date: 01/04/2017
+ * Time: 11:26.
+ */
+class Plugin implements ScraperInterface
+{
+    /**
+     * Goutte Client.
+     *
+     * @var
+     */
+    private $goutteClient;
+
+    /**
+     * An instance of Plugin Repository.
+     *
+     * @var PluginRepository
+     */
+    protected $plugin;
+
+    /**
+     * Plugin constructor.
+     *
+     * @param PluginRepository $plugin
+     */
+    public function __construct(PluginRepository $plugin)
+    {
+        $this->plugin       = $plugin;
+        $this->goutteClient = \App::make('goutte');
+    }
+
+    /**
+     * Scrape theme.
+     *
+     * @param int $page
+     */
+    public function scrape($page = 1)
+    {
+
+        $pageToCrawl = 'https://www.cssigniter.com/ignite/plugins/';
+
+        echo "Page: $pageToCrawl" . br();
+
+        $crawler = $this->goutteClient->request(
+            'GET',
+            $pageToCrawl
+        );
+
+        $plugin             = [];
+        $plugin['provider'] = 'cssigniter.com';
+        $plugin['type']     = 'premium';
+
+
+        $crawler->filter('div.item')
+                ->each(function (Crawler $pluginlist) use (&$plugin) {
+
+
+                    $plugin['name'] = $pluginlist->filter('h2.item-title')->text();
+
+
+                    $plugin['downloadlink'] = $pluginlist->filter('h2.item-title a')->attr('href');
+
+
+                    $uniqueidentifier = explode('/', $plugin['downloadlink']);
+
+                    $plugin['uniqueidentifier'] = $uniqueidentifier['5'];
+
+                    $plugin['screenshoturl'] = $pluginlist->filter('figure.item-thumb img')
+                                                          ->attr('src');
+
+
+                    try {
+                        // Navigate to the theme full page
+                        $crawlerPluginfullPage = $this->goutteClient->request(
+                            'GET',
+                            $plugin['downloadlink']
+                        );
+
+
+                        $plugin['description'] = trim($crawlerPluginfullPage
+                            ->filter('div.page-hero p')
+                            ->text());
+
+
+                    } catch (\Exception $e) {
+                        echo 'Could not scrape theme full page' . $e->getMessage() . br();
+                    }
+
+
+                    $plugin['previewlink'] = str_replace(
+                        'https://www.cssigniter.com/ignite/plugins/',
+                        'https://www.cssigniter.com/preview/',
+                        $plugin['downloadlink']);
+
+
+                    if ($this->plugin->save($plugin)) {
+                        echo '[' . getMemUsage() . ']' . $plugin['name'] . '(' . $plugin['uniqueidentifier'] . ')' . ' saved successfully';
+                    } else {
+                        echo '[' . getMemUsage() . ']' . $plugin['name'] . '(' . $plugin['uniqueidentifier'] . ')' . ' already exists in database.';
+                    }
+                    echo br();
+
+
+                    unset($plugin);
+
+                });
+
+
+    }
+}
---
+++ app/Scrape/CssIgniter/Theme.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Scrape\CssIgniter;
+
+use App\Scrape\ScraperInterface;
+use Symfony\Component\DomCrawler\Crawler;
+use App\Repositories\Theme\ThemeRepository;
+
+/**
+ * Created by PhpStorm.
+ * User: Hyder
+ * Date: 01/04/2017
+ * Time: 11:26.
+ */
+class Theme implements ScraperInterface
+{
+    /**
+     * Goutte Client.
+     *
+     * @var
+     */
+    private $goutteClient;
+
+    /**
+     * An instance of Theme Repository.
+     *
+     * @var ThemeRepository
+     */
+    protected $theme;
+
+    /**
+     * Theme constructor.
+     *
+     * @param ThemeRepository $theme
+     */
+    public function __construct(ThemeRepository $theme)
+    {
+        $this->theme        = $theme;
+        $this->goutteClient = \App::make('goutte');
+    }
+
+    /**
+     * Scrape theme.
+     *
+     * @param int $page
+     */
+    public function scrape($page = 1)
+    {
+
+        $pageToCrawl = 'https://www.cssigniter.com/ignite/themes/page/' . $page . '/';
+
+        echo "Page: $pageToCrawl" . br();
+
+        $crawler = $this->goutteClient->request(
+            'GET',
+            $pageToCrawl
+        );
+
+        $theme             = [];
+        $theme['provider'] = 'cssigniter.com';
+        $theme['type']     = 'premium';
+
+
+        $crawler->filter('div.item')
+                ->each(function (Crawler $themelist) use (&$theme) {
+
+
+                    $theme['name'] = $themelist->filter('h2.item-title')->text();
+
+
+                    $theme['downloadlink'] = $themelist->filter('h2.item-title a')->attr('href');
+
+
+                    $uniqueidentifier = explode('/', $theme['downloadlink']);
+
+                    $theme['uniqueidentifier'] = $uniqueidentifier['5'];
+
+                    $theme['screenshoturl'] = $themelist->filter('figure.item-thumb img')
+                                                        ->attr('src');
+
+
+                    try {
+                        // Navigate to the theme full page
+                        $crawlerThemefullPage = $this->goutteClient->request(
+                            'GET',
+                            $theme['downloadlink']
+                        );
+
+
+                        $theme['description'] = trim($crawlerThemefullPage
+                            ->filter('div.page-hero p')
+                            ->text());
+
+
+                    } catch (\Exception $e) {
+                        echo 'Could not scrape theme full page' . $e->getMessage() . br();
+                    }
+
+
+                    $theme['previewlink'] = str_replace(
+                        'https://www.cssigniter.com/ignite/themes/',
+                        'https://www.cssigniter.com/preview/',
+                        $theme['downloadlink']);
+
+
+                    if ($this->theme->save($theme)) {
+                        echo '[' . getMemUsage() . ']' . $theme['name'] . '(' . $theme['uniqueidentifier'] . ')' . ' saved successfully';
+                    } else {
+                        echo '[' . getMemUsage() . ']' . $theme['name'] . '(' . $theme['uniqueidentifier'] . ')' . ' already exists in database.';
+                    }
+                    echo br();
+
+
+                    unset($theme);
+
+                });
+
+
+    }
+}
