fe2df14885f4ccfbacb243d3074c455d77c94463
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-09-17 21:33:53 +0400

    Change from GET to POST

--- app/Http/Controllers/Extension/ExtensionController.php
+++ app/Http/Controllers/Extension/ExtensionController.php
@@ -12,10 +12,11 @@ class ExtensionController extends Controller
     public function scan()
     {
 
-        $site = \Request::post('url');
-
+        $site  = \Request::post('url');
         $debug = \Request::post('debug');
         $debug = is_null($debug) ? false : true;
+
+        //$input = \Request::all();
 
         $client   = new Client();
         $response = $client->request('GET', env('APP_URL') . '/site/?url=' . $site);
