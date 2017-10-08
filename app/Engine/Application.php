37fbbfa0db2f483f9481255fa66e76b804886c5e
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-10-08 16:01:58 +0400

    Refactoring

---
+++ app/Engine/Application.php
@@ -0,0 +1,190 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hyder
+ * Date: 08/10/2017
+ * Time: 14:39
+ */
+
+namespace App\Engine;
+
+use GuzzleHttp\Client;
+
+class Application
+{
+
+    /**
+     * Instance of  Illuminate\Http\Request
+     * @var
+     */
+    public $request;
+
+    public $response;
+
+    /**
+     * Application constructor.
+     *
+     * @param $request
+     */
+    public function __construct($request)
+    {
+        $this->request = $request;
+    }
+
+
+    /**
+     * Analyse data and find out what technologies is being used by a site
+     * @return mixed
+     */
+    public function analyze()
+    {
+
+        $debug = $this->request->input('debug');
+        $debug = is_null($debug) ? false : true;
+
+        $url         = $this->request->input('url');
+        $html        = $this->request->input('html');
+        $environment = $this->request->input('environment');
+        $headers     = $this->request->input('headers');
+
+        $responseFromExternalScan = $this->externalScan([
+            'mode' => 'online',
+            'url'  => $url,
+        ]);
+
+
+        //If there's no error
+        if ( ! isset($responseFromExternalScan->error)) {
+            $externalTechnologies = $technologies = $response = $responseFromExternalScan->technologies->applications;
+        }
+
+
+        if (isset($html)) {
+            $responseFromInternalScan = $this->internalScan([
+                'url'         => $url,
+                'html'        => $html,
+                'environment' => $environment,
+                'headers'     => $headers,
+                'status'      => 200,
+            ]);
+        }
+
+        $internalTechnologies = $response = $responseFromInternalScan->technologies->applications;
+
+        // Contains duplicate technlogies when combining offline and online scan
+        $technologies = array_merge((array)$internalTechnologies, (array)$externalTechnologies);
+
+        //Make technology list unique
+        $uniqueApplications = [];
+        if ( ! empty($technologies)) {
+            foreach ($technologies as $technology) {
+                $applicationName                      = $technology->name;
+                $uniqueApplications[$applicationName] = $technology;
+            }
+        }
+
+        if ( ! isset($responseFromExternalScan->error)) {
+            $responseFromExternalScan->technologies->applications = $uniqueApplications;
+            $response                                             = $responseFromExternalScan;
+        } else {
+
+            $responseFromInternalScan->application                = false;
+            $responseFromInternalScan->version                    = false;
+            $responseFromInternalScan->theme                      = false;
+            $responseFromInternalScan->plugins                    = false;
+            $responseFromInternalScan->technologies->applications = $uniqueApplications;
+            $response                                             = $responseFromInternalScan;
+        }
+
+        $applicationByCategory = $this->sortApplicationByCategory($response);
+
+        if ( ! empty($applicationByCategory)) {
+            $response->technologies->applications = $applicationByCategory;
+        }
+
+        return $response;
+    }
+
+    /**
+     * Run a real-time scan of the url
+     *
+     * @param $params
+     *
+     * @return mixed
+     */
+    public function externalScan($params)
+    {
+        // Fetch result from external call
+        $responseFromExternalScan = (new Client())->request(
+            'GET',
+            env('APP_URL') . '/site/?url=' . $params['url']
+        );
+
+        return json_decode($responseFromExternalScan->getBody()->getContents());
+
+    }
+
+    /**
+     * Run an offline scan without invoking the actual site.
+     *
+     * @param $params
+     *
+     * @return mixed
+     */
+    public function internalScan($params)
+    {
+        //Fetch result from internal togglyzer engine
+        $responseFromInternalScan = (
+        new Client()
+        )->request('POST', env('APP_URL') . '/site/ScanOfflineMode',
+                [
+                    'form_params' => [
+                        'url'         => $params['url'],
+                        'html'        => $params['html'],
+                        'environment' => $params['environment'],
+                        'headers'     => $params['headers'],
+                        'status'      => 200,
+                    ],
+                ]);
+
+
+        return json_decode($responseFromInternalScan->getBody()->getContents());
+
+    }
+
+
+    /**
+     *
+     * Group technologies by category
+     *
+     * @param $response
+     *
+     * @return array
+     */
+    public function sortApplicationByCategory($response)
+    {
+        $json                  = json_decode(file_get_contents(app_path() . '/../node_modules/togglyzer/apps.json'));
+        $categories            = $json->categories;
+        $applicationByCategory = [];
+
+        foreach ($categories as $category) {
+
+            $categoryName = $category->name;
+            if ( ! empty($response->technologies->applications)) {
+
+                foreach ($response->technologies->applications as $appName => $app) {
+                    $appCategories = $app->categories;
+
+                    if (in_array($categoryName, $appCategories)) {
+                        $applicationByCategory[$categoryName][] = $app;
+                    }
+
+                }
+
+            }
+        }
+
+        return $applicationByCategory;
+    }
+
+}
--- app/Http/Controllers/Extension/ExtensionController.php
+++ app/Http/Controllers/Extension/ExtensionController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\Extension;
 
+use App\Engine\Application;
 use App\Http\Controllers\Controller;
 use DB;
 use GuzzleHttp\Client;
-use Request;
-
+use Illuminate\Http\Request;
 
 class ExtensionController extends Controller
 {
@@ -86,106 +86,14 @@ class ExtensionController extends Controller
 
     }
 
-    public function scanv2()
+    public function scanv2(Request $request)
     {
-        $url   = Request::post('url');
-        $debug = Request::post('debug');
-        $debug = is_null($debug) ? false : true;
+        $application = new Application($request);
+        $response    = $application->analyze();
 
-        $html        = Request::post('html');
-        $environment = Request::post('environment');
-        $headers     = Request::post('headers');
-
-
-        // Fetch result from external call
-        $responseFromExternalScan = (new Client())->request(
-            'GET',
-            env('APP_URL') . '/site/?url=' . $url
-        );
-
-
-        $responseFromExternalScan = json_decode($responseFromExternalScan->getBody()->getContents());
-        $externalTechnologies     = $technologies = [];
-        //If there's no error
-        if ( ! isset($responseFromExternalScan->error)) {
-            $externalTechnologies = $technologies = $response = $responseFromExternalScan->technologies->applications;
-        }
-
-
-        if (isset($html)) {
-
-
-            //Fetch result from internal togglyzer engine
-            $responseFromInternalScan = (new Client())->request('POST', env('APP_URL') . '/site/ScanOfflineMode', [
-                'form_params' => [
-                    'url'         => $url,
-                    'html'        => $html,
-                    'environment' => $environment,
-                    'headers'     => $headers,
-                    'status'      => 200,
-                ],
-            ]);
-
-
-            $responseFromInternalScan = json_decode($responseFromInternalScan->getBody()->getContents());
-
-            $internalTechnologies = $response = $responseFromInternalScan->technologies->applications;
-
-            $technologies = array_merge((array)$internalTechnologies, (array)$externalTechnologies);
-
-        }
-
-
-        $uniqueApplications = [];
-        if ( ! empty($technologies)) {
-            foreach ($technologies as $technology) {
-                $applicationName                      = $technology->name;
-                $uniqueApplications[$applicationName] = $technology;
-            }
-        }
-
-
-        if ( ! isset($responseFromExternalScan->error)) {
-            $responseFromExternalScan->technologies->applications = $uniqueApplications;
-            $response                                             = $responseFromExternalScan;
-        } else {
-
-            $responseFromInternalScan->application                = false;
-            $responseFromInternalScan->version                    = false;
-            $responseFromInternalScan->theme                      = false;
-            $responseFromInternalScan->plugins                    = false;
-            $responseFromInternalScan->technologies->applications = $uniqueApplications;
-            $response                                             = $responseFromInternalScan;
-        }
-
-        $json                  = json_decode(file_get_contents(app_path() . '/../node_modules/togglyzer/apps.json'));
-        $categories            = $json->categories;
-        $applicationByCategory = [];
-
-
-        foreach ($categories as $category) {
-            $categoryName = $category->name;
-            if ( ! empty($response->technologies->applications)) {
-
-                foreach ($response->technologies->applications as $appName => $app) {
-                    $appCategories = $app->categories;
-
-
-                    if (in_array($categoryName, $appCategories)) {
-                        $applicationByCategory[$categoryName][] = $app;
-                    }
-
-                }
-
-            }
-        }
-
-        if ( ! empty($applicationByCategory)) {
-            $response->technologies->applications = $applicationByCategory;
-        }
 
         return view('extension/indexV2')
-            ->with('response', $response)
-            ->with('debug', $debug);
+            ->with('response', $response);
+
     }
 }
