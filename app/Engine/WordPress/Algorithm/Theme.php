5740248f526b837a8f9274a076d71cd7513a8e35
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-10-14 12:03:15 +0400

    remove previous regex

--- app/Engine/WordPress/Algorithm/Theme.php
+++ app/Engine/WordPress/Algorithm/Theme.php
@@ -93,7 +93,7 @@ class Theme extends WordPressAbstract
     private function extractThemeAlias($content)
     {
         $themes = [];
-        //if (preg_match_all('/\/wp-content\/themes\/(.+?)\//', $content, $matches)) {
+
         if (preg_match_all('/\/wp-content\/themes\/([A-Za-z0-9-.]+?)\//', $content, $matches)) {
 
             if ( ! empty($matches[1])) {
