d8a243fe848a8b6dbcaa56fa10012acbef1e30f0
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-10-15 23:43:13 +0400

    Added typed.js dependency + implementation

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,14 +11,12 @@ const {mix} = require('laravel-mix');
  |
  */
 
-//mix.js('resources/assets/js/app.js', 'public/js')
-
+mix.js('resources/assets/js/app.js', 'public/js');
 
 mix.sass('resources/assets/sass/app.scss', 'public/css')
   .options({
     processCssUrls: false
   });
-
 
 mix.copyDirectory('resources/assets/img', 'public/img');
 mix.copyDirectory('resources/assets/fonts', 'public/fonts');
