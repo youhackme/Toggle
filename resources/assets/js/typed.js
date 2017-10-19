71f3f6ebe4ce4b4c80950b9010db0946d77c72d6
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-10-19 21:12:25 +0400

    Load type.js only if there is a need to

--- resources/assets/js/typed.js
+++ resources/assets/js/typed.js
@@ -1,11 +1,13 @@
 $(function () {
 
-  new Typed('.typed', {
-    strings: ['Technologies', 'WordPress Theme', 'WordPress Plugins', 'Framework', 'Application', 'Fonts'],
-    typeSpeed: 100,
-    loop: true,
-    startDelay: 1000,
-    backDelay: 1500
-  });
+  if ($('span').hasClass('typed')) {
+    new Typed('.typed', {
+      strings: ['Technologies', 'WordPress Theme', 'WordPress Plugins', 'Framework', 'Application', 'Fonts'],
+      typeSpeed: 100,
+      loop: true,
+      startDelay: 1000,
+      backDelay: 1500
+    });
+  }
 
 });
