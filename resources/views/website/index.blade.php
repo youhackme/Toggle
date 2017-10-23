11bc9e0faf67875183d008d7d6fa1f5a072acec6
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-10-23 20:49:08 +0400

    A few minor improvements

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -50,15 +50,17 @@
                 </div>
                 <div class="col-md-offset-3 col-md-6 col-sm-12 col-xs-12">
                     <div class="blockSearch">
-                        <form role="form">
+                        <form method="get" action="/result">
                             <div class="input-group">
                                 <input type="text"
+                                       id="url"
+                                       name="url"
                                        class="form-control blockSearch__input-xlg"
                                        placeholder="https://toggle.me"
                                        autofocus>
                                 <span class="input-group-btn">
                       <button class="btn btn-default blockSearch__btn-search blockSearch__input-xlg"
-                              type="button">SEARCH
+                              type="submit">SEARCH
                       </button>
                     </span>
                             </div>
