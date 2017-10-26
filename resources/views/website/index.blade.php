0beefbf8ab158dcca7c6ef668bfb961e1d19982d
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-10-26 22:07:55 +0400

    move out navigation bar

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -4,39 +4,13 @@
 
 @section('content')
 
+
+    @include('website.includes.navigation', [
+  'logo' => 'img/toggle4.svg',
+  'homepage' => true
+  ])
+
     <div class="container-fluid">
-
-        <!--  Top Navigation  -->
-        <div class="row blockNavigation blockNavigation--full">
-            <nav class="navbar  navbar-static-top">
-                <div class="container">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                                data-target="#navbar3">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="{{env('APP_URL')}}">
-                            <img src="{{asset('img/toggle4.svg')}}" alt="Logo">
-                        </a>
-                    </div>
-                    <div id="navbar3" class="navbar-collapse collapse">
-                        <ul class="nav navbar-nav navbar-right">
-                            <li class="active"><a href="#">Home</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Contact</a></li>
-                        </ul>
-                    </div>
-                    <!--/.nav-collapse -->
-                </div>
-                <!--/.container-fluid -->
-            </nav>
-
-
-        </div>
-
         <!--  Headlines & Search bar  -->
         <div class="row">
             <div class="blockHeroWrapper">
