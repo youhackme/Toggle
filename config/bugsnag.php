70dd17de9d571aac704aee355c5b5387458d9846
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-12-04 22:18:27 +0400

    bump version to 1.0.8

--- config/bugsnag.php
+++ config/bugsnag.php
@@ -36,7 +36,7 @@ return [
     |
     */
 
-    'app_version' => env('BUGSNAG_APP_VERSION', '1.0.7'),
+    'app_version' => env('BUGSNAG_APP_VERSION', '1.0.8'),
 
     /*
     |--------------------------------------------------------------------------
