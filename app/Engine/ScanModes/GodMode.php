a64a956159117c92199d8bdd6f24d5820788f62c
Hyder Bangash <hyder.bangash@stellatelecom.com>
2018-01-01 21:40:18 +0400

    Initial working version of GodMode

--- app/Engine/ScanModes/GodMode.php
+++ app/Engine/ScanModes/GodMode.php
@@ -8,7 +8,6 @@
 
 namespace App\Engine\ScanModes;
 
-
 class GodMode extends \App\Engine\ApplicationAbstract
 {
 
@@ -20,16 +19,16 @@ class GodMode extends \App\Engine\ApplicationAbstract
     public function result()
     {
 
-        if ( ! $this->alreadyScanned()) {
-            echo('This url has NOT been scanned yet. ' . $this->request->getUrl());
-            // Then fetch live data
-            $search = (new ScanTechnologies($this->request))
-                ->setOptions(['mode' => 'online'])
-                ->search();
+        $responseHistoricalMode = new HistoricalMode($this->request);
 
-            $applicationsFromLiveSearch = $search->applications;
+        if ( ! $responseHistoricalMode->alreadyScanned()) {
+            //echo('This url has NOT been scanned yet. ' . $this->request->getUrl());
+            // Then fetch live data
+
+            $responseOnlineMode         = new OnlineMode($this->request);
+            $applicationsFromLiveSearch = $responseOnlineMode->applications;
             // Then fetch historical data by host
-            $applicationsFromHistoricalSearch = $this->searchForHistoricalTechnologies();
+            $applicationsFromHistoricalSearch = $responseHistoricalMode->searchForHistoricalTechnologies();
 
 
             $apps = new \App\Engine\Apps();
@@ -45,23 +44,22 @@ class GodMode extends \App\Engine\ApplicationAbstract
 
             $applications = $apps->unique();
 
-            //dd($applicationsFromHistoricalSearch, $applicationsFromLiveSearch);
+
             // Then merge it
         } else {
-            echo('This url has already beeen scanned ' . $this->request->getUrl());
+            //echo('This url has already beeen scanned ' . $this->request->getUrl());
             // Well this specific url has already been scanned before, no need to do a live search again.
             // Then fetch historical data by host
-            $applicationsFromHistoricalSearch = $this->searchForHistoricalTechnologies();
+            $applicationsFromHistoricalSearch = $responseHistoricalMode->searchForHistoricalTechnologies();
 
             $applications = $applicationsFromHistoricalSearch;
         }
-
-
         // Then fetch cached data by host name
         // Merge it with the live search.
         $this->applications = $applications;
 
         return $this;
     }
+
 
 }
