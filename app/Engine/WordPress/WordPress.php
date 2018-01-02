0fe20a35f029c6c99636fb43a12f18097714450a
Hyder Bangash <hyder.bangash@stellatelecom.com>
2018-01-02 18:50:03 +0400

    The link algorithm needs improvement

--- app/Engine/WordPress/WordPress.php
+++ app/Engine/WordPress/WordPress.php
@@ -32,7 +32,7 @@ class WordPress
         \App\Engine\WordPress\Algorithm\Theme::class,
         \App\Engine\WordPress\Algorithm\Plugin::class,
         \App\Engine\WordPress\Algorithm\Robot::class,
-        \App\Engine\WordPress\Algorithm\Link::class,
+        //\App\Engine\WordPress\Algorithm\Link::class,
         \App\Engine\WordPress\Algorithm\Screenshot::class,
         \App\Engine\WordPress\Algorithm\Version::class,
 
